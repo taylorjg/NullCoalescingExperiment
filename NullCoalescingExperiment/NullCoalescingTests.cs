fc36c5d1fb6e7daa2fa6197a05a6b846a812b65c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-06-11 20:35:50 +0100

    Initial commit

---
+++ NullCoalescingExperiment/NullCoalescingTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NullCoalescingExperiment
+{
+    // ReSharper disable UnusedVariable
+    // ReSharper disable NotAccessedField.Local
+
+    public class Thing
+    {
+        public string Prop1 { get; set; }
+        public int Prop2 { get; set; }
+    }
+
+    [TestFixture]
+    internal class NullCoalescingTests
+    {
+        private static List<Thing> GetThings()
+        {
+            return new List<Thing>
+                {
+                    new Thing {Prop1 = "ABC", Prop2 = 123},
+                    new Thing {Prop1 = "DEF", Prop2 = 456}
+                };
+        }
+
+        private List<Thing> _things;
+
+        [Test]
+        public void DebuggerStrangenessInvolvingTheNullCoalescingOperatorAndAMemberVariable()
+        {
+            _things = GetThings() ?? new List<Thing>();
+            var flag = true;
+        }
+    }
+}
